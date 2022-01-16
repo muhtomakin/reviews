a0d8a44f673543551c8b02e6736c98ab87df1d3d
muhtomakin <muhtomakin@gmail.com>
2022-01-16 16:37:25 +0100

    freecodecamp react projects

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.scss';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
